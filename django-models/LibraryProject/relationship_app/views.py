d4ba62c98e00fef0866a2f298aac6ecfcb0d0eef
Kwame Takyi <takyiemmanuelkwame@gmail.com>
2025-09-03 13:02:43 +0000

    Correct view.py of relationship.app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView   
 from .models import Book
-from .models import Library   
+from .models import Library
 
--
+
+
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
