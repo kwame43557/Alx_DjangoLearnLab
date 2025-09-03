bfb8369f130bbb219ee10ea7927f74427699fe0d
Kwame Takyi <takyiemmanuelkwame@gmail.com>
2025-09-03 07:05:24 +0000

    Made more correction in the relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,8 @@ from .models import Book, Library
 # Function-based view
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
 
 class LibraryDetailView(DetailView):
     model = Library
