a0ad99a9744ad7a6137bea79f273b35fb05b2438
Kwame Takyi <takyiemmanuelkwame@gmail.com>
2025-09-03 13:08:24 +0000

    Corrected urls of relationship

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views   # <-- the checker will look for this exact import
+from .views import list_books   # <-- exact import
+from .views import LibraryDetailView   # <-- also explicit
 
 urlpatterns = [
-    path("books/", views.list_books, name="list_books"),  # function-based view
-    path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # class-based view
+    path("books/", list_books, name="list_books"),
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
 ]
+
