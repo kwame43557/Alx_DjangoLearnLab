75d4ea7895385e8430c37d01574d92ed817cec36
Kwame Takyi <takyiemmanuelkwame@gmail.com>
2025-09-03 17:46:48 +0000

    Assignment 2 second commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,13 @@
-﻿from django.urls import path
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
+
+    # built-in class-based login view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+
+    # built-in class-based logout view
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
